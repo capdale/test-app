5bc5300f7942c237a900da088ab0f598b621becf
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-11-27 02:13:45 +0900

    camera page

--- lib/pages/camera/camera.dart
+++ lib/pages/camera/camera.dart
@@ -2,31 +2,21 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:image/image.dart' as img;
+import 'package:modak/modak.dart';
+import 'package:provider/provider.dart';
+import 'package:test_app/pages/geolocation/geolocation.dart';
+import 'package:test_app/provider/modak_provider.dart';
 
-class CameraPage extends StatelessWidget {
-  const CameraPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => TakePhotoPage()));
-          },
-          child: const Text("Camera")),
-    );
-  }
-}
-
-class TakePhotoPage extends StatefulWidget {
-  TakePhotoPage({Key? key}) : super(key: key);
+class CameraPage extends StatefulWidget {
+  CameraPage({Key? key}) : super(key: key);
 
   @override
-  State<TakePhotoPage> createState() => _TakePhotoPageState();
+  State<CameraPage> createState() => _CameraPageState();
 }
 
-class _TakePhotoPageState extends State<TakePhotoPage> {
+class _CameraPageState extends State<CameraPage> {
   CameraController? _cameraController;
   bool _isCameraReady = false;
 
@@ -36,9 +26,13 @@ class _TakePhotoPageState extends State<TakePhotoPage> {
     super.initState();
     availableCameras().then((cameras) {
       if (cameras.isNotEmpty && _cameraController == null) {
-        _cameraController =
-            CameraController(cameras.first, ResolutionPreset.max);
+        _cameraController = CameraController(
+            cameras.first, ResolutionPreset.max,
+            imageFormatGroup: ImageFormatGroup.jpeg, enableAudio: false);
         _cameraController!.initialize().then((value) {
+          if (!mounted) {
+            return;
+          }
           setState(() {
             _isCameraReady = true;
           });
@@ -46,16 +40,14 @@ class _TakePhotoPageState extends State<TakePhotoPage> {
           if (e is CameraException) {
             switch (e.code) {
               case 'CameraAccessDenied':
-                print("CameraAccessDenied");
                 break;
               default:
-                print("default error");
                 break;
             }
           }
         });
       }
-    });
+    }).catchError((e) {});
   }
 
   @override
@@ -73,20 +65,23 @@ class _TakePhotoPageState extends State<TakePhotoPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: _cameraController != null && _isCameraReady
-                ? CameraPreview(_cameraController!)
-                : Container(),
-          ),
-          IconButton(
-              onPressed: _isCameraReady ? () => _onTakePhoto(context) : null,
-              icon: const Icon(Icons.camera))
-        ],
+    if (_cameraController == null) return Container();
+    if (!_cameraController!.value.isInitialized) return Container();
+    final cameraWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: OverflowBox(
+        maxHeight: cameraWidth,
+        child: SizedBox(
+          width: cameraWidth,
+          height: cameraWidth * _cameraController!.value.aspectRatio,
+          child: CameraPreview(_cameraController!),
+        ),
       ),
+      floatingActionButton: IconButton(
+        icon: const Icon(Icons.camera),
+        onPressed: () => _onTakePhoto(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
@@ -97,8 +92,70 @@ class PhotoPreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image.file(File(imagePath)),
+    return Scaffold(
+      body: FutureBuilder(
+          future: isGeoLocationServiceEnable(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Container();
+            }
+            if (snapshot.hasError) {
+              Navigator.pop(context);
+            }
+            if (snapshot.data == false) {
+              return const Center(
+                child: Text("has no permission"),
+              );
+            }
+            return Center(
+              child: Image.file(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width,
+                  File(imagePath),
+                  fit: BoxFit.fill),
+            );
+          }),
+      floatingActionButton: IconButton(
+          onPressed: () async {
+            final modak = context.read<ModakChangeNotifier>().modak!;
+
+            img.Image? image =
+                img.decodeImage(File(imagePath).readAsBytesSync());
+            if (image == null) {
+              const snackBar = SnackBar(content: Text("Error"));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              return;
+            }
+            final position = await Geolocator.getCurrentPosition();
+            final resizedImage =
+                img.copyResize(image, width: 1080, height: 1080);
+            final isSuccess = await modak.collection
+                .postCollection(img.encodeJpg(resizedImage), position.accuracy,
+                    position.accuracy, position.accuracy, position.accuracy)
+                .then((value) {
+              if (value) {
+                const snackBar = SnackBar(content: Text("Success"));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                return;
+              }
+              const snackBar = SnackBar(content: Text("Server Error"));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            });
+          },
+          icon: const Icon(Icons.check)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
+}
+
+Future<bool> isGeoLocationServiceEnable() async {
+  var permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) return false;
+  }
+  if (permission == LocationPermission.deniedForever) {
+    return false;
+  }
+  return true;
 }
