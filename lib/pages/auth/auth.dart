c64b95b415fa164c4ec4941f0146f5248f3e33f0
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-11-27 02:13:22 +0900

    Github OAuth Page

--- lib/pages/auth/auth.dart
+++ lib/pages/auth/auth.dart
@@ -1,28 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:test_app/lib/github_auth.dart';
+import 'package:modak/modak.dart';
+import 'package:test_app/provider/setting_provider.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: TextButton(
-        child: const Text("github"),
-        onPressed: () async {
-          await Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => Scaffold(
-                body: GithubOAuth2Widget(
-                  loginURL: "http://10.0.2.2:8080/auth/github/login",
-                  callbackURL: "http://localhost:8080/auth/github/callback",
-                  onCallback: null,
-                ),
-              ),
-            ),
-          );
-        },
+    void testCallback(Token token) async {
+      print(token.uuid);
+    }
+
+    final setting = context.read<SettingsChangeNotifier>().settings;
+    return Material(
+      child: GithubOAuth2Widget(
+        loginURL: "http://${setting.host}:8080/auth/github/login?type=json",
+        callbackURL: "http://${setting.host}:8080/auth/github/callback",
+        onCallback: testCallback,
+        httpClient: HttpClient(),
       ),
     );
   }
